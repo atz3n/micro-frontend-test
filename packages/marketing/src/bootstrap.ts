cd16994c3c391ab249c566795229152668c67010
atz3n <attila.aldemir@atz3n.dev>
2022-11-15 13:11:17 +0100

    improve code style

--- packages/marketing/src/bootstrap.ts
+++ packages/marketing/src/bootstrap.ts
@@ -3,10 +3,10 @@ import { mount } from './mount';
 
 
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
-  if (devRoot) {
-    mount(devRoot, { defaultHistory: createBrowserHistory() });
-  }
+    const devRoot = document.querySelector('#_marketing-dev-root');
+    if (devRoot) {
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
+    }
 }
 
 export { mount };
